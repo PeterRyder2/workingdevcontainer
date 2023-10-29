5bdc57ae12b6d16958657ed45ff4eeb1da90154e
Peter Ryder <peter.ryder2@gmail.com>
2023-10-29 23:41:53 +0000

    dont mount volumne in devcontainer docker compose. it was scresing things up giving me file exist errors

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ import os
 
 db_password = os.environ.get("MYSQL_ROOT_PASSWORD")
 db_nanme = os.environ.get("MYSQL_DB_NAME")
-SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://root:{db_password}@db/{db_nanme}"
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://root:{db_password}@db/bleeper"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
